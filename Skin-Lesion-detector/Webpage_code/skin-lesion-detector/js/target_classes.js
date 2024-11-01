0fb01c3dc0d1ca7036c6f4d35beacfe2a7a5e782
ashwiniudupa <ashudupa@gmail.com>
2024-10-31 17:46:03 -0700

    Skin Lesion detection

---
+++ Skin-Lesion-detector/Webpage_code/skin-lesion-detector/js/target_classes.js
@@ -0,0 +1,10 @@
+const TARGET_CLASSES = {
+    0: 'Actinic Keratosis(akiec)',
+    1: 'Basal Cell Carcinoma(bcc)',
+    2: 'Benign Keratosis(bkl)',
+    3: 'Dermatofibroma(df)',
+    4: 'Melanoma(mel)',
+    5: 'Melanocytic Nevi(nv)',
+    6: 'Vascular skin lesion(vasc)'
+  
+  };
